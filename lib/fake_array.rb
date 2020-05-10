a008f376c1ad8d0109c1d0043b38fb7e22217465
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-09 22:43:01 -0400

    Fakes em  out

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,7 +1,19 @@
-class FakeArray
-
   # you'll need a splat in this class somewhere
+require "pry"
+class FakeArray
+  include Enumerable
 
+  attr_reader :array
 
+  def initialize(*array)
+    @array = array
+  end
 
+  def [](i)
+    @array[i]
+  end
+
+  def each(&block)
+    @array.each(&block)
+  end
 end
